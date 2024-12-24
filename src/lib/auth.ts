0332511ea78afa91df18ab2ee5ab1529f469a06d
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-12-24 17:25:57 -0500

    create auth functions

---
+++ src/lib/auth.ts
@@ -0,0 +1,68 @@
+import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from '@oslojs/encoding';
+import { authSessions, users, type AuthSession, type User } from './db/schema';
+import { sha256 } from '@oslojs/crypto/sha2';
+import { eq } from 'drizzle-orm';
+
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
+
+export async function createSession(
+  token: string,
+  userId: number,
+  locals: App.Locals
+): Promise<AuthSession> {
+  const { db } = locals;
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: AuthSession = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
+  };
+  await db.insert(authSessions).values(session);
+  return session;
+}
+
+export async function validateSessionToken(
+  token: string,
+  locals: App.Locals
+): Promise<SessionValidationResult> {
+  const { db } = locals;
+
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await db
+    .select({ user: users, session: authSessions })
+    .from(authSessions)
+    .innerJoin(users, eq(authSessions.userId, users.cid))
+    .where(eq(authSessions.id, sessionId));
+  if (result.length < 1) {
+    return { session: null, user: null };
+  }
+  const { user, session } = result[0];
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await db.delete(authSessions).where(eq(authSessions.id, session.id));
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await db
+      .update(authSessions)
+      .set({
+        expiresAt: session.expiresAt
+      })
+      .where(eq(authSessions.id, session.id));
+  }
+  return { session, user };
+}
+
+export async function invalidateSession(sessionId: string, locals: App.Locals): Promise<void> {
+  const { db } = locals;
+  await db.delete(authSessions).where(eq(authSessions.id, sessionId));
+}
+
+export type SessionValidationResult =
+  | { session: AuthSession; user: User }
+  | { session: null; user: null };
