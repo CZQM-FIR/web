5c549334941330b0a87018afbbd05e99621a3cbc
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-12-25 22:00:01 -0500

    add static pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,193 @@
   import '../app.css';
   import type { Snippet } from 'svelte';
   import type { LayoutData } from './$types';
+  import Icon from '@iconify/svelte';
+  import { page } from '$app/stores';
+
+  import CZQMLogo from '$lib/assets/images/CZQM-White.svg';
 
   let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
 
-{@render children()}
+<!-- nav bar -->
+<nav class="w-screen bg-base-100">
+  <div class="container navbar top-0 mx-auto">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            /></svg
+          >
+        </div>
+        <ul class="menu dropdown-content menu-sm z-50 mt-3 rounded-box bg-base-100 p-2 shadow">
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <details>
+              <summary>Controllers</summary>
+              <ul class="p-2">
+                <li><a href="/staff"><Icon icon="mdi:administrator" /> Staff</a></li>
+                <li>
+                  <a href="/roster"><Icon icon="mdi:account-group" /> Roster</a>
+                </li>
+                <li><a href="/join"><Icon icon="mdi:account-add" /> Join Us</a></li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <details>
+              <summary>Pilots</summary>
+              <ul class="p-2">
+                <li>
+                  <a href="/pilot-resources"><Icon icon="mdi:clipboard-text" /> Resources</a>
+                </li>
+                <li><a href="/charts"><Icon icon="mdi:map" /> Charts</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a href="/events">Events</a></li>
+          <li><a href="/news">News</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </div>
+      <a href="/" class="btn btn-ghost text-xl">CZQM / QX</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <div class="dropdown dropdown-bottom">
+            <div tabindex="0" role="button">Controllers</div>
+            <ul class="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
+              <li><a href="/staff"><Icon icon="mdi:administrator" /> Staff</a></li>
+              <li>
+                <a href="/roster"><Icon icon="mdi:account-group" /> Roster</a>
+              </li>
+              <li><a href="/join"><Icon icon="mdi:account-add" /> Join Us</a></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div class="dropdown dropdown-bottom">
+            <div tabindex="0" role="button">Pilots</div>
+            <ul class="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
+              <li>
+                <a href="/pilot-resources"><Icon icon="mdi:clipboard-text" /> Resources</a>
+              </li>
+              <li><a href="/charts"><Icon icon="mdi:map" /> Charts</a></li>
+            </ul>
+          </div>
+        </li>
+        <li><a href="/events">Events</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      {#if data.user}
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost">
+            <Icon icon="mdi:account" />
+            {data.user.name_full}
+          </div>
+          <ul class="menu dropdown-content z-[1] rounded-box bg-base-100 p-2 shadow">
+            <li><a href="/my"><Icon icon="mdi:account-edit" /> MyCZQM</a></li>
+            <li>
+              <a href={`/auth/logout`}><Icon icon="mdi:logout" /> Logout</a>
+            </li>
+          </ul>
+        </div>
+      {:else}
+        <a href={`/auth?redirect=${$page.url.pathname}`} class="btn"
+          ><Icon icon="mdi:login-variant" /> Login</a
+        >
+      {/if}
+    </div>
+  </div>
+
+  <!-- Headline -->
+  <!-- {#if $page.data.headline.article || $page.data.headline.event}
+    <div id="news-header" class="bg-primary text-center p-0.5">
+      <h1 class="mx-auto font-semibold">
+        {#if $page.data.headline.event}
+          <a class="hover:link" href="/events/{$page.data.headline.event._id}"
+            ><span class="hidden md:inline">Upcoming Event:</span>
+            {$page.data.headline.event.name}</a
+          >
+        {/if}
+
+        {#if $page.data.headline.event && $page.data.headline.article}
+          <span class="text-base-100"> | </span>
+        {/if}
+
+        {#if $page.data.headline.article}
+          <a class="hover:link" href="/news/{$page.data.headline.article._id}"
+            >{$page.data.headline.article.name}</a
+          >
+        {/if}
+      </h1>
+    </div>
+  {/if} -->
+</nav>
+
+<!-- main content -->
+<main class="min-h-screen">
+  {@render children()}
+</main>
+
+<!-- footer -->
+<footer class="bg-base-200 py-10 text-base-content md:p-10">
+  <div class="footer max-md:footer-center md:container md:mx-auto">
+    <aside>
+      <img src={CZQMLogo} alt="" class="hidden md:block" />
+      <p>
+        <strong>Moncton Gander FIR</strong><br />Not affiliated with Nav Canada.<br />For flight
+        simulation use only.
+      </p>
+    </aside>
+    <nav>
+      <h6 class="footer-title">Policies</h6>
+      <a href="/privacy" class="link-hover link">Privacy Policy</a>
+      <a href="/branding" class="link-hover link">Branding</a>
+      <a href="/sop" class="link-hover link">SOPs</a>
+    </nav>
+    <nav>
+      <h6 class="footer-title">Quick Links</h6>
+      <a href="/about" class="link-hover link">About Us</a>
+      <a href="https://wiki.czqm.ca/en/faqs" class="link-hover link">FAQs</a>
+      <a href="/join" class="link-hover link">Join Us</a>
+      <a href="/contact" class="link-hover link">Contact Us</a>
+      <a href="https://wiki.czqm.ca" class="link-hover link">Wiki</a>
+    </nav>
+    <nav>
+      <h6 class="footer-title">Connect With Us</h6>
+      <a href="https://twitter.com/czqm_fir" class="link-hover link flex items-center"
+        ><Icon icon="mdi:twitter" class="mr-1" /> Twitter</a
+      >
+      <a href="https://www.facebook.com/CZQMFIR/" class="link-hover link flex items-center"
+        ><Icon icon="mdi:facebook" class="mr-1" /> FaceBook</a
+      >
+      <a
+        href="https://www.youtube.com/channel/UCS5H_U3h6edXWashMjQpuAg"
+        class="link-hover link flex items-center"
+        ><Icon icon="mdi:youtube" class="mr-1" /> Youtube</a
+      >
+      <a href="/portal/discord" class="link-hover link flex items-center"
+        ><Icon icon="ic:baseline-discord" class="mr-1" /> Discord</a
+      >
+    </nav>
+  </div>
+</footer>
---
+++ src/routes/charts/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  import ChartExampleImage from '$lib/assets/images/fundy5.png';
+  interface Props {
+    data: PageData;
+  }
+
+  let { data = $bindable() }: Props = $props();
+</script>
+
+<section id="charts-info" class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src={ChartExampleImage}
+      alt="NAVCAN Chart for the FUNDY5 arrival into Halifax"
+      class="image"
+    />
+    <div>
+      <h1 class="text-5xl font-bold">What about charts?</h1>
+      <p class="">
+        Unfortunatly, we are unable to provide pilots with charts at this time. There are many
+        different places where you can access charts for free online.
+      </p>
+      <br />
+      <p>
+        The best option for IFR charts is <a
+          href="https://navigraph.com"
+          target="_blank"
+          class="link">Navigraph</a
+        > which provides Jeppesen charts and FMS data for a monthly subscription.
+      </p>
+      <br />
+      <p>
+        The <a href="https://www.navcanada.ca/en/ecfs_07_en.pdf" target="_blank" class="link"
+          >Canada Flight Supplement</a
+        > is a great option for accessing airport charts and procedure charts for all airports across
+        Canada.
+      </p>
+      <br />
+      <p class="">
+        For airport charts, <a href="https://fltplan.com" target="_blank" class="link"
+          >fltplan.com</a
+        > is a great resource and has all Canadian charts availible with a free account. They even include
+        moving charts if you use their app!
+      </p>
+      <br />
+      <p>
+        For VFR sectionals, <a href="https://skyvector.com" target="_blank" class="link"
+          >Skyvector</a
+        > has the majority of Canadian VFR charts
+      </p>
+    </div>
+  </div>
+</section>
+
+<style>
+  .image {
+    width: 40%;
+    height: auto;
+    transform: rotate(-90deg);
+  }
+</style>
