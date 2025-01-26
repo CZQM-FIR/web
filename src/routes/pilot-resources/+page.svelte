7df7cffbb1bc67e94fcfcf8f331b60690a96cfe9
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-01-26 09:34:18 -0500

    add pilot resources

---
+++ src/routes/pilot-resources/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<section id="resources" class="min-h-screen">
+  <div class="container mx-auto">
+    <h1 class="mt-6 text-2xl">Pilot resources</h1>
+    <div class="divider"></div>
+    <div class="flex flex-col gap-3">
+      {#each data.resources as resource}
+        <a href={resource.url} target="_blank">
+          <div class="card bg-base-200 shadow-xl">
+            <div class="card-body">
+              <div class="flex flex-row items-end gap-3">
+                <h2 class="card-title">{resource.name}</h2>
+                <div class="badge badge-neutral">
+                  {resource.category[0].toUpperCase()}{resource.category.slice(1)}
+                </div>
+              </div>
+              <p>{resource.description}</p>
+            </div>
+          </div>
+        </a>
+      {/each}
+    </div>
+  </div>
+</section>
+
+<p>{JSON.stringify(data.resources)}</p>
