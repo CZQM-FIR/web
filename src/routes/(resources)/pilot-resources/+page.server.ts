1e9b4e25a457fcd8442b3474b51507f5b247e09e
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-05-11 13:01:52 -0400

    add auth

--- src/routes/(resources)/pilot-resources/+page.server.ts
+++ src/routes/(resources)/pilot-resources/+page.server.ts
@@ -5,10 +5,7 @@ import { db } from '$lib/czqm/db';
 export const load = (async ({}) => {
   const pilotResources = await db.query.resources.findMany({
     where: (resources, { eq, and, or }) =>
-      and(
-        or(eq(resources.type, 'controller'), eq(resources.type, 'both')),
-        eq(resources.public, 1)
-      ),
+      and(or(eq(resources.type, 'pilot'), eq(resources.type, 'both')), eq(resources.public, 1)),
     columns: {
       public: false,
       type: false
